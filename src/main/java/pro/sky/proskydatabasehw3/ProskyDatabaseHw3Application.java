71588ebb8ddeb946e8d1cec25cb9b778e8e8e227
Obmor <terekhin.alex@mail.ru>
2025-02-18 12:49:36 +0300

    TheFirstLook

---
+++ src/main/java/pro/sky/proskydatabasehw3/ProskyDatabaseHw3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.proskydatabasehw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProskyDatabaseHw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProskyDatabaseHw3Application.class, args);
+	}
+
+}
